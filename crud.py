cc7c4d3293354e82e3eb6eaa82b9e41ae7464a1f
Reeja20040923 <reejayaramalla@gmail.com>
2026-03-03 14:40:05 +0530

    CRUD (delete)

--- crud.py
+++ crud.py
@@ -57,3 +57,11 @@ def update_book(book_id:int,book_update: Book_Update):
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
 
 
+@app.delete("/books/{book_id}")
+def delete_book(book_id:int):
+    for book in books:
+        if book["id"] == book_id:
+            books.remove(book)
+            return {"Message": "Book deleted successfully"}
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail="Book not found")
+    
